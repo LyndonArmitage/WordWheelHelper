a6c38a0fcf30b5395c7b8b79cf9a303f1e6e7265
Lyndon Armitage <lyndon.armitage@gmail.com>
2013-01-09 19:43:39 +0000

    Some Formatting stuff and added TextNode

---
+++ src/com/lyndonarmitage/WordWheel/helpers/TextNode.java
@@ -0,0 +1,13 @@
+package com.lyndonarmitage.WordWheel.helpers;
+
+/**
+ * Created By: Lyndon Armitage
+ * Date: 09/01/13
+ */
+public class TextNode {
+
+	private char addedCharacter = '\0';
+	private String textString = "";
+	private TextNode parent = null;
+
+}
