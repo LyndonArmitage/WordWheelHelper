0ff66eb8633bc7564de96bbb990abec391dedf2d
Lyndon Armitage <lyndon.armitage@gmail.com>
2013-01-09 19:33:24 +0000

    Initial Commit. I am new to git so this could be interesting.

---
+++ src/com/lyndonarmitage/WordWheel/Main.java
@@ -0,0 +1,14 @@
+package com.lyndonarmitage.WordWheel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lyndon
+ * Date: 09/01/13
+ * Time: 19:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+}
